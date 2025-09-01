94ce940813ccea52151f7f9da16fc17c60db2253
farza <farzanrichu610@gmail.com>
2025-09-01 20:12:05 +0530

    Formatting

--- projectile_functions.py
+++ projectile_functions.py
@@ -6,4 +6,3 @@ class Projectile_Functions():
         self.ax, self.ay = 0, 0  # accelerations
         self.G = 9.81  # gravity (m/s^2)
         self.air_resistance = False  # toggle (can connect to GUI)
-
