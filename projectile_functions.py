444d29af3454b1e9a8d1f5363612d3981c5c1401
farza <farzanrichu610@gmail.com>
2025-09-01 21:13:29 +0530

    deleted some comments

--- projectile_functions.py
+++ projectile_functions.py
@@ -4,5 +4,5 @@ class Projectile_Functions():
         self.vx, self.vy = 0, 0  # velocity components
         self.x, self.y = 0, 0  # position
         self.ax, self.ay = 0, 0  # accelerations
-        self.G = 9.81  # gravity (m/s^2)
-        self.air_resistance = False  # toggle (can connect to GUI)
+        self.G = 9.81  # gravity
+        self.air_resistance = False
