ae16725ca0dcdc7282132f623ee262467dc885d3
farza <farzanrichu610@gmail.com>
2025-09-02 03:17:38 +0530

    input validation and passing it as defaults

--- main_window.py
+++ main_window.py
@@ -78,8 +78,23 @@ class MainWindow(QMainWindow):
         self.timer.timeout.connect(self.update_position)
 
     def launch_btn(self):
+        try:
+            angle = float(self.angle_input.text())
+        except ValueError:
+            QMessageBox.warning(self, "Warning", "Please enter a valid angle (degrees)")
+            return
+
+        try:
+            speed = float(self.speed_input.text())
+        except ValueError:
+            QMessageBox.warning(self, "Warning", "Please enter a valid speed")
+            return
+
+        if angle == 0 or speed == 0:
+            self.projectile.default_condtions(speed=speed, deg_ang=angle)
+
         if self.air_resistance:
-            self.points = self.projectile.with_air_resistance()
+            self.points = self.projectile.with_air_resistance(speed,deg_ang)
         else:
             self.points = self.projectile.without_air_resistance()
 
