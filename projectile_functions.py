ac567d97513b9994bee28b75d8e088fcd968978d
farza <farzanrichu610@gmail.com>
2025-09-02 02:08:57 +0530

    set the most basic equations

--- projectile_functions.py
+++ projectile_functions.py
@@ -1,3 +1,4 @@
+import math
 class Projectile_Functions():
     def __init__(self):
         self.v = 0  # initial speed
@@ -6,6 +7,13 @@ class Projectile_Functions():
         self.ax, self.ay = 0, 0  # accelerations
         self.G = 9.81  # gravity
         self.air_resistance = False
+
+    def default_condtions(self,speed,deg_ang=45):
+        launch_angle=math.radians(deg_ang)
+        self.v=speed
+        self.vx=speed*math.cos(launch_angle)
+        self.vx=speed*math.cos(launch_angle) #basic equations
+        self.x, self.y = 0, 0
 
     def with_air_resistance(self):
         pass
