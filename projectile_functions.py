52f0e2212c703fbb701f717d9fe31191103fafa9
farza <farzanrichu610@gmail.com>
2025-09-03 02:53:24 +0530

    Made more object oriented

--- projectile_functions.py
+++ projectile_functions.py
@@ -3,38 +3,37 @@ import math
 
 class Projectile_Functions():
     def __init__(self):
-        self.v = 0  # initial speed
-        self.vx, self.vy = 0, 0  # velocity components
-        self.x, self.y = 0, 0  # position
-        self.ax, self.ay = 0, 0  # accelerations
-        self.G = 9.81  # gravity
-        self.air_resistance = False
-        self.TIME_STEP = 0.05
+        v = 0  # initial speed
+        vx, vy = 0, 0  # velocity components
+        x, y = 0, 0  # position
+        ax, ay = 0, 0  # accelerations
+        G = 9.81  # gravity
+        air_resistance = False
+        TIME_STEP = 0.05
 
+    @staticmethod
     def with_air_resistance(self, dt=0.05, speed=30, deg_ang=45, m=1, k=0.1):
         points = []
+        G = 9.81
         angle = math.radians(deg_ang)
-        self.vx = speed * math.cos(angle)
-        self.vy = speed * math.sin(angle)
-        self.x, self.y = 0, 0
+        vx = speed * math.cos(angle)
+        vy = speed * math.sin(angle)
+        x, y = 0, 0
 
-        while self.y >= 0:
-            ax = - (k / m) * self.vx
-            ay = -self.G - (k / m) * self.vy
+        while y >= 0:
+            ax = - (k / m) * vx
+            ay = -G - (k / m) * vy
 
-            self.vx += ax * dt
-            self.vy += ay * dt
-            self.x += self.vx * dt
-            self.y += self.vy * dt
+            vx += ax * dt
+            vy += ay * dt
+            x += vx * dt
+            y += vy * dt
 
-            points.append((self.x, self.y))
+            points.append((x,y))
 
         return points
-
-    def stats(self):
-        v0 = self.v
-        theta = self.angle
-        g = self.G
+    @staticmethod
+    def stats(v0, theta, g=9.81):
 
         # default equations
         T = (2 * v0 * math.sin(theta)) / g
@@ -48,19 +47,20 @@ class Projectile_Functions():
             "Range": round(R, 3),
             "Horizontal Velocity": round(vx, 3),
         }
-
+    @staticmethod
     def without_air_resistance(self, dt=0.05,speed=30, deg_ang=45):
+        G = 9.81
         points = []
         angle = math.radians(deg_ang)
-        self.vx = speed * math.cos(angle)
-        self.vy = speed * math.sin(angle)
-        self.x, self.y = 0, 0
-        self.ax = 0
-        self.ay = -self.G
-        while self.y >= 0:
-            self.vx += self.ax * dt
-            self.vy += self.ay * dt
-            self.x += self.vx * dt
-            self.y += self.vy * dt
-            points.append((self.x, self.y))
+        vx = speed * math.cos(angle)
+        vy = speed * math.sin(angle)
+        x, y = 0, 0
+        ax = 0
+        ay = -G
+        while y >= 0:
+            vx += ax * dt
+            vy += ay * dt
+            x += vx * dt
+            y += vy * dt
+            points.append((x, y))
         return points
